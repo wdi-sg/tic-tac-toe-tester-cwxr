99ae9daa56059034f768c8484640b9aa73b43118
robin chua <robinchua@gmail.com>
2017-03-23 16:05:28 +0800

    submission

--- testing-the-tester.js
+++ testing-the-tester.js
@@ -1,35 +1,38 @@
 var player = 1
-var grid = [null,null,null,null,null,null,null,null,null]
+var grid = [null, null, null, null, null, null, null, null, null]
 
+function playTurn (index) {
+  if (grid[index] || isGameOver()) {
+    return false
+  } else {
+    grid[index] = player
+    if (player === 1) {
+      player = 2    // why ! is not needed? -- if !(player === 1) player = 2
+    } else player = 1
+    return true
+  }
+}
 
-function playTurn(index) {
+  // check for winning conditions
 
-  if(grid[index] || isGameOver()) {
+function isGameOver () {
+  if (whoWon()) {
+    return true
+  } else {
     return false
   }
-    else {
-      grid[index] = player
-      if (player === 1) player = 2    //why ! is not needed? -- if !(player === 1) player = 2
-      else player = 1
-      return true
-    }
 }
 
-function restart() {
+function restart () {
   player = 1
-  grid = [null,null,null,null,null,null,null,null,null]
-
+  grid = [null, null, null, null, null, null, null, null, null]
 }
 
-function isGameOver() {
-  if (whoWon())
-    return true
-else
-    return false
+// input: nothing
+// output: either 1, 2, or 3
+// or: 0 if its the start of the game
 
-}
-
-function whoWon() {
+function whoWon () {
   if (grid[0] && grid[0] === grid[1] && grid[0] === grid[2]) return grid[0]
   if (grid[3] && grid[3] === grid[4] && grid[3] === grid[5]) return grid[3]
   if (grid[6] && grid[6] === grid[7] && grid[6] === grid[8]) return grid[6]
